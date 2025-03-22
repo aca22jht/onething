5a5db6d36d79e7094c26d6985f7669af40e9475a
aca22jht <JHPTay1@sheffield.ac.uk>
2025-03-22 22:24:10 +0000

    door and chest for level2

--- Assets/DoorController.cs
+++ Assets/DoorController.cs
@@ -6,19 +6,31 @@ public class DoorController : MonoBehaviour
 {
     public TopDownCharacterController characterController;
 
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
-    {
+    { // 3 levels in total
+        string sceneN = SceneManager.GetActiveScene().name;
         Debug.Log(characterController.levelComplete);
+
         if (characterController.levelComplete)
         {
-            SceneManager.LoadScene("Level2");
-        }
-        
+            if (sceneN == "Level1")
+            {
+                SceneManager.LoadScene("Level2"); // go through level 1 door to level 2
+            }
+            else if (sceneN == "Level2")
+            {
+                SceneManager.LoadScene("Level3"); // go through level 1 door to level 2
+            }
+            else if (sceneN == "Level2")
+            {
+                SceneManager.LoadScene("Level3"); // go through level 1 door to level 2
+            }
+        }        
     }
 }
