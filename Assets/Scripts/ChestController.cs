4e3438b056fece6aecf2e9ce97d762b03c003871
ChristinaW44 <103203583+ChristinaW44@users.noreply.github.com>
2025-03-22 19:51:48 +0000

    made chest open

--- Assets/Scripts/ChestController.cs
+++ Assets/Scripts/ChestController.cs
@@ -1,20 +1,37 @@
+using Cainos.PixelArtTopDown_Basic;
 using UnityEngine;
 
 public class ChestController : MonoBehaviour
 {
     [SerializeField] private GameObject puzzleBox;
     [SerializeField] private GameObject afterPuzzleUI;
+    [SerializeField] private GameObject chestClosed;
+    [SerializeField] private GameObject chestOpen;
+    public TopDownCharacterController characterController;
 
     private void Start()
     {
+        chestOpen.SetActive(false);
+        chestClosed.SetActive(true);
         puzzleBox.SetActive(false);
         afterPuzzleUI.SetActive(false);
     }
 
+    private void Update()
+    {
+        if (characterController.levelComplete) {
+            chestClosed.SetActive(false);
+            chestOpen.SetActive(true) ;
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("collision");
-        puzzleBox.SetActive(true);
+        Debug.Log(characterController.levelComplete);
+        if (characterController.levelComplete == false) {
+            puzzleBox.SetActive(true);
+        }
+        
     }
 
 }
